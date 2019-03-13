6c024f7d627ce7e01b513e67d311c0637a8bd59b
victor <1045606768@qq.com>
2019-03-13 22:08:17 +0800

    vuex读取icon列表

--- src/store.js
+++ src/store.js
@@ -3,7 +3,23 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+// https://www.cnblogs.com/yuzhongyu/p/10451947.html
+// 检索icon目录
+const context = require.context('../public/img/icons', false, /.png$/);
+const moduleStores = {};
+context.keys().forEach(key => {
+  const fileName = key.slice(2, -3);
+  const fileModule = context(key).default;
+  moduleStores[fileName] = {
+    ...fileModule,
+    namespaced: true,
+  };
+});
+
 export default new Vuex.Store({
+  modules: {
+    ...moduleStores,
+  },
   state: {
 
   },
