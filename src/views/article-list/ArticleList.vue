1db2fb4a01b662e94594def390b17688ee775348
coolucifer <1045606768@qq.com>
2019-03-14 17:42:51 +0800

    添加新页面

---
+++ src/views/article-list/ArticleList.vue
@@ -0,0 +1,33 @@
+<!-- 帖子列表 -->
+<template>
+  <page-frame class="article-list">
+    <page-header slot="header"></page-header>
+  </page-frame>
+</template>
+
+<script>
+import PageFrame from '@/components/PageFrame.vue';
+import PageHeader from '@/components/PageHeader.vue';
+
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+    PageFrame,
+    PageHeader,
+  },
+  computed: {
+  },
+  watch: {
+  },
+  mounted() {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
