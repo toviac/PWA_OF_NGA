c63cb6ab0ba673105be64165ac9348781ac5c647
coolucifer <1045606768@qq.com>
2019-03-18 13:13:51 +0800

    添加momentjs及全局filter

--- src/main.js
+++ src/main.js
@@ -1,11 +1,35 @@
 import Vue from 'vue';
 import './cube-ui';
+import moment from 'moment';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 import http from './common/http';
 import urls from './common/urls';
+
+
+moment.locale('zh-cn');
+// 全局时间filter
+Vue.filter('formatDate', (timestamp, pattern) => {
+  if (pattern) {
+    return moment(timestamp * 1000).format(pattern);
+  }
+  const duration = moment.duration(moment(timestamp * 1000) - moment());
+  switch (true) {
+    case duration.years() < -1:
+      return moment(timestamp * 1000).format('YYYY-MM-DD HH:mm');
+    case duration.days() < -1:
+      return moment(timestamp * 1000).format('MM-DD HH:ss');
+    case duration.hours() < -1:
+      return moment(timestamp * 1000).format('今天 HH:ss');
+    case duration.minutes() > -1:
+      return '刚刚';
+    default:
+      break;
+  }
+  return duration.humanize(true).replace(/ /g, '');
+});
 
 Vue.config.productionTip = false;
 
