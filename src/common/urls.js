ba68fbe3fbe14cbcde047cecb27a806b3c330e15
victor <1045606768@qq.com>
2019-04-16 20:56:15 +0800

    添加个人收藏板块接口

--- src/common/urls.js
+++ src/common/urls.js
@@ -2,6 +2,8 @@ const urls = {
   getCategory: '/app_api.php?__lib=home&__act=category',
   getList: '/app_api.php?__lib=subject&__act=list',
   getPost: '/app_api.php?__lib=post&__act=list',
+  // app_id=1010&sign=24aa64fe37a4acea76994c7ee32dd1d1&access_uid=10956343&t=1555418247&access_token=24bfdefe0631e9a9868560ed0ec7e63c0691837a
+  getFavorForums: '/app_api.php?__lib=favorforum&__act=sync&',
 };
 
 export default urls;
