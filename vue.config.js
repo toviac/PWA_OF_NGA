f9b9a82e7f27504ae7fe7db018288d35f374c702
coolucifer <1045606768@qq.com>
2019-04-16 08:31:55 +0800

    打包文件夹改为nga

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   // 基本路径
   publicPath: '/',
   // 输出文件目录
-  outputDir: 'dist',
+  outputDir: 'nga',
   // eslint-loader 是否在保存的时候检查
   lintOnSave: true,
   // use the full build with in-browser compiler?
