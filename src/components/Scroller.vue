821a53edc82b7a34275f467a2ec20d3e42470e73
coolucifer <1045606768@qq.com>
2019-04-30 17:09:08 +0800

    update scroller

--- src/components/Scroller.vue
+++ src/components/Scroller.vue
@@ -1,14 +1,14 @@
 <!-- 上拉刷新/下拉加载 -->
 <template>
-  <div class="scroller" :style="{ 'height': height, 'transform': `translate3D(0px,${offset}px,0px)` }">
+  <div class="scroller" :style="scrollerStyle">
     <div class="pull-down-wrapper">
-      <span>REFRESH...</span>
+      <span>{{ refreshTxt }}</span>
     </div>
     <div class="scroll-wrapper" @touchmove="touchHandler" @touchend="touchEndHandler">
       <slot></slot>
     </div>
     <div class="pull-up-wrapper">
-      <span>LOAD MORE...</span>
+      <span>{{ loadTxt }}</span>
     </div>
   </div>
 </template>
@@ -21,6 +21,10 @@ export default {
       type: Boolean,
       default: false,
     },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
     height: {
       type: String,
       default: '100%',
@@ -30,73 +34,105 @@ export default {
     return {
       touchArr: [],
       isRefreshing: false,
+      isLoading: false,
+      offset: 0,
     };
   },
   components: {
   },
   computed: {
-    offset() {
-      const { touchArr, isRefreshing, isLoading } = this;
-      let scrollOffset = 0;
-      if (touchArr.length) {
-        scrollOffset = touchArr[touchArr.length - 1] - touchArr[0];
+    scrollerStyle() {
+      const { height, offset } = this;
+      let style = {
+        height,
+        transform: `translate3D(0px,${offset}px,0px)`,
+      };
+      if (!offset || offset === 50) {
+        style = Object.assign({}, style, {
+          transition: 'all 0.2s ease 0s',
+        });
       }
-      switch (true) {
-        case isRefreshing:
-          return 50;
-        case isLoading:
-          return -50;
-        case Math.abs(scrollOffset) > 100:
-          if (scrollOffset > 0) {
-            return 100;
-          }
-          return -100;
-        default:
-          return scrollOffset;
+      return style;
+    },
+    refreshTxt() {
+      const { offset, isRefreshing } = this;
+      if (isRefreshing || !offset) return 'REFRESHING...';
+      if (offset > 50) {
+        return 'RELEASE TO REFRESH';
       }
+      return 'PULL DOWN TO REFRESH';
+    },
+    loadTxt() {
+      const { offset, isLoading } = this;
+      if (isLoading || !offset) return 'LOADING...';
+      if (offset < -50) {
+        return 'RELEASE TO LOAD MORE';
+      }
+      return 'PULL UP TO LOAD MORE';
     },
   },
   watch: {
     refreshing(newVal) {
       this.isRefreshing = newVal;
     },
+    loading(newVal) {
+      this.isLoading = newVal;
+    },
+    isRefreshing(newVal) {
+      if (!newVal) {
+        this.offset = 0;
+      }
+    },
+    isLoading(newVal) {
+      if (!newVal) {
+        this.offset = 0;
+      }
+    },
   },
   mounted() {
   },
   methods: {
     touchHandler(e) {
-      const { offset } = this;
+      const {
+        offset,
+        touchArr,
+        // isLoading,
+        // isRefreshing,
+      } = this;
       const el = document.querySelector('.scroll-wrapper');
       // scrollTop === 0时滚动到顶部
       const isTop = !el.scrollTop;
       const isBottom = el.scrollHeight - el.scrollTop === el.clientHeight;
-      const firstTouch = this.touchArr[0];
+      // const firstTouch = touchArr[0];
+      // console.log('==> ', isTop, isBottom, e.changedTouches[0].pageY);
       // (在顶端&&下拉) || (在底端&&上拉)
-      if ((isTop && (firstTouch || 0) < e.changedTouches[0].pageY) || (isBottom && (firstTouch || el.scrollHeight) > e.changedTouches[0].pageY)) {
-        console.log('==> ', isTop, isBottom, firstTouch, e.changedTouches[0].pageY);
+      if ((isTop && offset >= 0) || (isBottom && offset <= 0)) {
         if (offset) {
           e.preventDefault();
         }
-        // 触摸点相对于浏览器的viewport上边缘的y坐标, 不会包括上边的滚动距离
-        this.touchArr.push(e.changedTouches[0].clientY);
+        if (touchArr.length < 2 || offset) {
+          touchArr.push(e.changedTouches[0].pageY);
+        }
       }
+      this.setOffset();
     },
     touchEndHandler(e) {
       const { offset } = this;
       console.log('touchEnd: ', e);
       // 下拉
-      if (offset > 0) {
+      if (offset > 50) {
         this.pullDown();
-      } else {
+      } else if (offset < -50) {
         this.pullUp();
       }
       this.touchArr = [];
+      this.setOffset();
     },
     // 下拉
     pullDown() {
       console.log('pullDown');
       this.$emit('pull-down');
-      // this.isRefreshing = true;
+      this.isRefreshing = true;
       setTimeout(() => {
         this.isRefreshing = false;
       }, 5000);
@@ -105,13 +141,45 @@ export default {
     pullUp() {
       console.log('pullUp');
     },
+    setOffset() {
+      const { touchArr, isRefreshing, isLoading } = this;
+      // 触摸偏移
+      let touchOffset = 0;
+      // 上次偏移
+      let lastOffset = 0;
+      if (touchArr.length) {
+        touchOffset = (touchArr[touchArr.length - 1] - touchArr[0]) / 2;
+        lastOffset = ((touchArr[touchArr.length - 2] || touchArr[0]) - touchArr[0]) / 2;
+      }
+      switch (true) {
+        case isRefreshing:
+          this.offset = 50;
+          break;
+        case isLoading:
+          this.offset = -50;
+          break;
+        // 限制最大滑动长度
+        // case Math.abs(touchOffset) > 100:
+        //   if (touchOffset > 0) {
+        //     return 100;
+        //   }
+        //   return -100;
+        default:
+          // 防止随意拉动时产生不正确的offset
+          // e.g. 在最上方向下拉, 再迅速向上拉时, 会产生一个负的offset, 导致页面上移一部分
+          if (touchOffset * lastOffset < 0) {
+            this.offset = 0;
+          }
+          this.offset = touchOffset;
+          break;
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss">
 .scroller {
-  transition: all 0 ease-out;
   .pull-down-wrapper,
   .pull-up-wrapper {
     background-color: #ccc;
